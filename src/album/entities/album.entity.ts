e37bf659958852e7e6392ca92fe207075bc6b159
adevksh <alekseydevksh@gmail.com>
2025-12-07 22:14:06 +0100

    feat: convert entities to TypeORM entities

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,8 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
+import { Artist } from '../../artist/entities/artist.entity';
+import { Track } from '../../track/entities/track.entity';
+
+@Entity('albums')
 export class Album {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'text' })
   name: string;
+
+  @Column({ type: 'int' })
   year: number;
+
+  @ManyToOne(() => Artist, (artist) => artist.albums, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'artistId' })
+  artist: Artist;
+
+  @Column({ type: 'uuid', nullable: true, default: null })
   artistId: string | null;
+
+  @OneToMany(() => Track, (track) => track.album, { cascade: false })
+  tracks: Track[];
 
   constructor(partial: Partial<Album>) {
     Object.assign(this, partial);
--- src/artist/entities/artist.entity.ts
+++ src/artist/entities/artist.entity.ts
@@ -1,7 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Album } from '../../album/entities/album.entity';
+import { Track } from '../../track/entities/track.entity';
+
+@Entity('artists')
 export class Artist {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'text' })
   name: string;
+
+  @Column({ type: 'boolean' })
   grammy: boolean;
+
+  @OneToMany(() => Album, (album) => album.artist, { cascade: false })
+  albums: Album[];
+
+  @OneToMany(() => Track, (track) => track.artist, { cascade: false })
+  tracks: Track[];
 
   constructor(partial: Partial<Artist>) {
     Object.assign(this, partial);
